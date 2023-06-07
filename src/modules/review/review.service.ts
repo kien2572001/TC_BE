b9b4c75be88cf56346497677f0db5cc71b8c49ee
dunglee <ledung76d@gmail.com>
2023-06-08 00:31:24 +0700

    feat: api get data review user

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Review } from 'src/database/entity/review.entity';
 import { Repository } from 'typeorm';
+import { V1GetReviewsByUserIdParamDto } from './dto/get-reviews-by-user-id.dto';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class ReviewService {
   constructor(
     @InjectRepository(Review)
     private reviewRepository: Repository<Review>,
+    private userService: UserService,
   ) {}
   async getAvgRating(id, type): Promise<any> {
     let reviewRaw;
@@ -69,6 +72,30 @@ export class ReviewService {
     const result = {
       rating,
       reviews: reviewRaw,
+    };
+    return result;
+  }
+
+  async getReviewsByUserId(param: V1GetReviewsByUserIdParamDto): Promise<any> {
+    const { userId } = param;
+    const id = parseInt(userId, 10);
+
+    const user = await this.userService.getUserById(id);
+    if (!user) {
+      return { user: {}, review: [] };
+    }
+    const reviewsRaw = await this.reviewRepository.find({
+      where: { userId: id },
+    });
+    const userData = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+    };
+    const result = {
+      user: userData,
+      review: reviewsRaw,
     };
     return result;
   }
