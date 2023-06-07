2b4523ce5563657bd2ba666cf2a3a22f93c05c71
dunglee <ledung76d@gmail.com>
2023-06-07 20:43:54 +0700

    feat: fix eslint reviews

--- src/modules/review/review.module.ts
+++ src/modules/review/review.module.ts
@@ -3,15 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
 import { Review } from 'src/database/entity/review.entity';
-import { FoodsController } from './foods/foods.controller';
-import { FoodsService } from './foods/foods.service';
-import { FoodsModule } from './foods/foods.module';
-import { FoodsModule } from './foods/foods.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review]), FoodsModule],
-  controllers: [ReviewController, FoodsController],
-  providers: [ReviewService, FoodsService],
+  imports: [TypeOrmModule.forFeature([Review])],
+  controllers: [ReviewController],
+  providers: [ReviewService],
   exports: [ReviewService],
 })
 export class ReviewModule {}
