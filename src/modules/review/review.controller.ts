278360cf53fa19ecfa52b72d8f135a4a192f0931
viet <viet7896@gmail.com>
2023-06-21 23:43:45 +0700

    feat: Create food review

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -10,7 +10,8 @@ import { Public } from 'src/decorator/public.decorator';
 import { V1GetReviewsByUserIdParamDto } from './dto/get-reviews-by-user-id.dto';
 import { Roles } from 'src/decorator/roles.decorator';
 import { ERole } from 'src/core/enum/default.enum';
-import { V1PostRestaurantReviewBodyDto } from './dto/post-restaurant-review.dto';
+import { V1PostReviewBodyDto } from './dto/post-review.dto';
+
 @ApiBearerAuth()
 @ApiTags('API Reviews')
 @Controller('reviews')
@@ -43,15 +44,28 @@ export class ReviewController {
   }
 
   @Roles([ERole.USER])
-  @Post('restaurant/:restaurantId')
+  @Post('restaurants/:restaurantId')
   @ApiBearerAuth('BearerAuth')
   @ApiOperation({ summary: 'Create restaurant review' })
   @ApiResponse({ status: 403, description: 'Forbidden.' })
-  createRestaurant(
-    @Body() body: V1PostRestaurantReviewBodyDto,
+  createRestaurantReview(
+    @Body() body: V1PostReviewBodyDto,
     @Param() param,
     @Request() req,
   ) {
     return this.reviewService.createRestaurantReview(body, param, req);
+  }
+
+  @Roles([ERole.USER])
+  @Post('foods/:foodId')
+  @ApiBearerAuth('BearerAuth')
+  @ApiOperation({ summary: 'Create food review' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  createFoodReview(
+    @Body() body: V1PostReviewBodyDto,
+    @Param() param,
+    @Request() req,
+  ) {
+    return this.reviewService.createFoodReview(body, param, req);
   }
 }
